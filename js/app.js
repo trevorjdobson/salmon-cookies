61a9cce7899250d3480eb232c5da76690cfc74d2
trevorjdobson <trevorjdobson@gmail.com>
2019-06-11 10:58:36 -0700

    refactored with constructor

--- js/app.js
+++ js/app.js
@@ -1,111 +1,30 @@
 'use strict';
 
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+let allStoresArr = [];
 
-// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+function Store(location, minCustomerPerHour,maxCustomerPerHour,avgCookiesPerCustomer){
+  this.location = location;
+  this.minCustomerPerHour = minCustomerPerHour;
+  this.maxCustomerPerHour = maxCustomerPerHour;
+  this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+  this.cookiesPerHour = [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}];
+  this.totalCookies = null;
 
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+  generateAllSales(this);
+  allStoresArr.push(this);
+  appendData(this);
+}
 
-// Store the results for each location in a separate array... perhaps as a property of the object representing that location
+function generateRandom(min,max){
+  var num = Math.floor(Math.random() * (max - min + 1)) + min;
 
-// Display the values of each array as unordered lists in the browser
+  return num;
+}
 
-// Calculating the sum of these hourly totals; your output for each location should look like this:
-
-// 1st and Pike
-
-// 6am: 16 cookies
-// 7am: 20 cookies
-
-var firstPike = {
-  location: 'First and Pike',
-  minCustomerPerHour: 23,
-  maxCustomerPerHour: 65,
-  avgCookiesPerCustomer: 6.3,
-  cookiesPerHour: [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}],
-  totalCookies: null,
-  generateRandom: function(){
-    var min = this.minCustomerPerHour;
-    var max = this.maxCustomerPerHour;
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return num;
-  },
-
-};
-
-var seaTac = {
-  location: 'Seatac',
-  minCustomerPerHour: 3,
-  maxCustomerPerHour: 24,
-  avgCookiesPerCustomer: 1.2,
-  cookiesPerHour: [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}],
-  totalCookies: null,
-  generateRandom: function(){
-    var min = this.minCustomerPerHour;
-    var max = this.maxCustomerPerHour;
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return num;
-  },
-
-};
-
-var seattleCenter = {
-  location: 'Seattle Center',
-  minCustomerPerHour: 11,
-  maxCustomerPerHour: 24,
-  avgCookiesPerCustomer: 3.7,
-  cookiesPerHour: [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}],
-  totalCookies: null,
-  generateRandom: function(){
-    var min = this.minCustomerPerHour;
-    var max = this.maxCustomerPerHour;
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return num;
-  },
-
-};
-
-var capitolHill = {
-  location: 'Capitol Hill',
-  minCustomerPerHour: 20,
-  maxCustomerPerHour: 38,
-  avgCookiesPerCustomer: 2.3,
-  cookiesPerHour: [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}],
-  totalCookies: null,
-  generateRandom: function(){
-    var min = this.minCustomerPerHour;
-    var max = this.maxCustomerPerHour;
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return num;
-  },
-
-};
-
-var alki = {
-  location: 'Alki',
-  minCustomerPerHour: 2,
-  maxCustomerPerHour: 16,
-  avgCookiesPerCustomer: 4.6,
-  cookiesPerHour: [{time:'6am'},{time:'7am'},{time:'8am'},{time:'9am'},{time:'10am'},{time:'11am'},{time:'12pm'},{time:'1pm'},{time:'2pm'},{time:'3pm'},{time:'4pm'},{time:'5pm'},{time:'6pm'},{time:'7pm'},{time:'8pm'}],
-  totalCookies: null,
-  generateRandom: function(){
-    var min = this.minCustomerPerHour;
-    var max = this.maxCustomerPerHour;
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return num;
-  },
-
-};
-
-//Hours open === 15
 function generateAllSales(store){
+  console.log(store);
   for(var i = 0; i < store.cookiesPerHour.length; i++){
-    let cookies = Math.floor(store.generateRandom() * store.avgCookiesPerCustomer);
+    let cookies = Math.floor(generateRandom(store.minCustomerPerHour,store.maxCustomerPerHour) * store.avgCookiesPerCustomer);
     store.cookiesPerHour[i].sales = cookies;
     store.totalCookies += cookies;
   }
@@ -133,21 +52,10 @@ function appendData(store){
   salesData.appendChild(totalSalesItem);
 }
 
-let allStoresArr = [firstPike, alki,seaTac,seattleCenter,capitolHill];
+//Create new stores
+var alki = new Store('Alki', 2, 16, 4.6);
+var seaTac = new Store('Seatac', 3, 24, 1.2);
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+var seattleCenter = new Store('Seattle Center', 11, 24, 3.7);
+var firstPike = new Store('First & Pike',23, 65, 6.3);
 
-function renderAll(arr){
-  for(var i = 0; i < arr.length; i++){
-    generateAllSales(arr[i]);
-    appendData(arr[i]);
-  }
-}
-renderAll(allStoresArr);
-// generateAllSales(alki);
-// generateAllSales(seaTac);
-// generateAllSales(seattleCenter);
-// generateAllSales(capitolHill);
-// generateAllSales(firstPike);
-
-// appendData(firstPike);
-
-// console.log(alki,seattleCenter,seaTac,capitolHill,firstPike);
